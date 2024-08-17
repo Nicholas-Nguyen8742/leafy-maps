0cf0a6aca53bb2295fdff43ca2c7c0bab16d205a
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-08-16 23:15:59 -0500

    [Plugin] - Add external link capability within figma iframe

--- packages/plugin/src/plugin.ts
+++ packages/plugin/src/plugin.ts
@@ -2,3 +2,9 @@ figma.showUI(__html__, {
   width: 800,
   height: 560
 });
+
+figma.ui.onmessage = (message) => {
+  if (message.type === 'OPEN_IN_BROWSER') {
+    figma.openExternal(message.url)
+  }
+}
