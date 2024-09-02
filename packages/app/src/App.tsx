a77bf8c7e23350de4c75ea2e9c8893ba4f710ac1
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-02 16:40:12 -0500

    [App] - Add dimensions text to show current width and height dimensions configured by users

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -4,6 +4,19 @@ import MapContainer from './components/map/Container';
 import FormContainer from './components/forms/Container';
 import 'leaflet/dist/leaflet.css';
 import './styles/App.css';
+import { useElementDimensions } from './hooks';
+
+function DimensionsLabel() {
+  const { width, height } = useElementDimensions('map');
+  console.log(useElementDimensions('map'));
+  return (
+    <div id='distance' className='distance-container'>
+      {width && height && (
+        <pre>{`${width}px by ${height}px`}</pre>
+      )}
+    </div>
+  );
+}
 
 export function App() {
   const [leafletMap, setLeafletMap] = useState<L.Map | undefined>();
@@ -18,9 +31,11 @@ export function App() {
       </span>
     ) : (
       <>
-        <div style={{ width: '75%' }}>
+        <div style={{ position: 'relative', width: '75%' }}>
+          {leafletMap && (<DimensionsLabel />)}
           <MapContainer map={leafletMap} onMapUpdate={setLeafletMap} />
-        </div><div style={{ width: '25%', overflowY: 'scroll', margin: '8px 8px 8px 16px' }}>
+        </div>
+        <div style={{ width: '25%', overflowY: 'scroll', margin: '8px 8px 8px 16px' }}>
           <FormContainer map={leafletMap} onGenerate={setIsLoading} />
         </div>
       </>
