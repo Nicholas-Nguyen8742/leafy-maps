44e74fb1eecfd0f0be451aeacba06a3d75ba991a
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-02 18:16:39 -0500

    [Plugin] - Implement logic to calculate new message type - INTERNAL_RESIZE_MAP

--- packages/plugin/src/plugin.ts
+++ packages/plugin/src/plugin.ts
@@ -1,11 +1,21 @@
 /* eslint-disable no-case-declarations */
 
+const WIDTH = 1280;
+const HEIGHT = 900;
+
 figma.loadAllPagesAsync();
 
 figma.showUI(__html__, {
-  width: 1280,
-  height: 900
+  width: WIDTH,
+  height: HEIGHT
 });
+
+function calculateDimension(original: number, newValue: number, actual: number) {
+  const diff = (original < newValue)
+    ? Math.abs(original - newValue)
+    : -(Math.abs(original - newValue));
+  return actual + diff;
+}
 
 figma.ui.onmessage = (message) => {
   switch(message?.type) {
@@ -34,6 +44,19 @@ figma.ui.onmessage = (message) => {
         figma.closePlugin();
       });
 
+      break;
+
+    case 'INTERNAL_RESIZE_MAP':
+      console.log({ type: 'INTERNAL_RESIZE_MAP', message })
+      const { width, height } = message;
+      const originalWidth = 930;
+      const originalHeight = 884;
+
+      const calculatedWidth = calculateDimension(originalWidth, parseInt(width), WIDTH);
+      const calculatedHeight = calculateDimension(originalHeight, parseInt(height), HEIGHT);
+
+      console.log('calculatedWidth: ', calculatedWidth);
+      figma.ui.resize(calculatedWidth, calculatedHeight);
       break;
   }
 }
